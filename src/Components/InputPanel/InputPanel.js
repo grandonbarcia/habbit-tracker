1def0e5a09dcb179f255c242474b6eb74131d602
Brandon Garcia <grandonleebarcia@gmail.com>
2021-05-31 08:00:47 -0700

    lifted logic to parent component and passed as props

--- src/Components/InputPanel/InputPanel.js
+++ src/Components/InputPanel/InputPanel.js
@@ -1,43 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React from 'react'
 import { Row, Col, Form, Button } from 'react-bootstrap'
-import useForm from '../../Hooks/useForm'
 import HabbitToggleList from './HabbitToggleList';
 
-const InputPanel = ({ saveData, forceUpdate, update }) => {
-
-    const [todaysDate, setTodaysDate] = useState('');
-
-    const switchRef = useRef();
-
-    const { handleChange, formData, setFormData } = useForm();
-
-    const [toggleSwitch, setSwitch] = useState(false);
-
-    const handleClick = () => {
-        console.log(switchRef.current.name);
-        const timeElapsed = Date.now();
-        const today = new Date(timeElapsed);
-        console.log(today.toDateString());
-        localStorage.setItem(today.toDateString(), JSON.stringify(formData))
-        forceUpdate(!update)
-
-    }
-
-
-    useEffect(() => {
-
-        const timeElapsed = Date.now();
-        const today = new Date(timeElapsed);
-        setTodaysDate(today.toDateString());
-
-    }, [])
-
-
-
-
+const InputPanel = ({ saveData, handleClick, habitsTracked, handleChange, setHabitsTracked, displayDate }) => {
 
     return (
-
         <Col className="h-100 shadow-lg" xl={3}>
             <Row className="text-center">
                 <Col>
@@ -46,12 +13,12 @@ const InputPanel = ({ saveData, forceUpdate, update }) => {
             </Row>
             <Row className="text-center">
                 <Col>
-                    <h3>{todaysDate}</h3>
+                    <h3>{displayDate}</h3>
                 </Col>
             </Row>
             <Row>
                 <Col className="Form__col">
-                    <Row>
+                    {/* <Row>
                         <Col>
                             <Form>
                                 <Form.Group controlId="formBasicRange">
@@ -60,14 +27,14 @@ const InputPanel = ({ saveData, forceUpdate, update }) => {
                                 </Form.Group>
                             </Form>
                         </Col>
-                    </Row>
-                    <HabbitToggleList handleChange={handleChange} toggleSwitch={toggleSwitch} switchRef={switchRef} />
+                    </Row> */}
+                    <HabbitToggleList handleChange={handleChange} habitsTracked={habitsTracked} setHabitsTracked={setHabitsTracked} />
                     <Row>
                         <Col>
                             <Form>
                                 <Form.Group controlId="exampleForm.ControlTextarea1">
                                     <Form.Label>Journal: </Form.Label>
-                                    <Form.Control as="textarea" rows={3} name="journal" onChange={handleChange} />
+                                    <Form.Control as="textarea" rows={3} name="journal" onChange={handleChange} value={habitsTracked.journal} />
                                 </Form.Group>
                             </Form>
                         </Col>
@@ -79,7 +46,6 @@ const InputPanel = ({ saveData, forceUpdate, update }) => {
                     </Row>
                 </Col>
             </Row>
-
         </Col>
 
     )
