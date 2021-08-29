a7a76d47650743fd1b187292c32a3ed3351a6801
Brandon Garcia <grandonleebarcia@gmail.com>
2021-08-29 13:42:28 -0700

    reformatted code with prettier

--- src/Components/InputPanel/HabbitToggleList.js
+++ src/Components/InputPanel/HabbitToggleList.js
@@ -1,62 +1,78 @@
-import React from 'react'
-import { Row, Col, Form } from 'react-bootstrap'
+import React from "react";
+import { Row, Col, Form } from "react-bootstrap";
 
+const HabbitToggleList = ({
+  handleChange,
+  habitsTracked,
+  setHabitsTracked,
+}) => {
+  return (
+    <>
+      <Row>
+        <Col>
+          <Form.Switch
+            id="Excercise"
+            label="Excercise 1hr"
+            name="excercise"
+            defaultChecked={habitsTracked.excercise}
+            onClick={() =>
+              setHabitsTracked({
+                ...habitsTracked,
+                excercise: !habitsTracked.excercise,
+              })
+            }
+          />
+        </Col>
+        <Col>
+          <Form.Check
+            type="switch"
+            id="Stretch"
+            label="Stretch 10 mins"
+            name="stretch"
+            defaultChecked={habitsTracked.stretch}
+            onClick={() =>
+              setHabitsTracked({
+                ...habitsTracked,
+                stretch: !habitsTracked.stretch,
+              })
+            }
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Form.Check
+            type="switch"
+            id="Meditate"
+            label="Meditate 20 mins"
+            name="meditate"
+            defaultChecked={habitsTracked.meditate}
+            onClick={() =>
+              setHabitsTracked({
+                ...habitsTracked,
+                meditate: !habitsTracked.meditate,
+              })
+            }
+          />
+        </Col>
+        <Col>
+          <Form.Check
+            type="switch"
+            id="Coffee"
+            label="Drink Coffee"
+            name="coffee"
+            defaultChecked={habitsTracked.coffee}
+            onClick={() =>
+              setHabitsTracked({
+                ...habitsTracked,
+                coffee: !habitsTracked.coffee,
+              })
+            }
+          />
+        </Col>
+      </Row>
+    </>
+  );
+};
 
-const HabbitToggleList = ({ handleChange, habitsTracked, setHabitsTracked }) => {
-
-    return (
-        <div>
-
-            <Row>
-                <Col>
-                    <Form.Switch
-                        id="Excercise"
-                        label="Excercise 1hr"
-                        name="excercise"
-
-                        checked={habitsTracked.excercise}
-                        onClick={() => setHabitsTracked({ ...habitsTracked, excercise: !habitsTracked.excercise })}
-                    />
-                </Col>
-                <Col>
-                    <Form.Check
-                        type="switch"
-                        id="Stretch"
-                        label="Stretch 10 mins"
-                        name="stretch"
-
-                        checked={habitsTracked.stretch}
-                        onClick={() => setHabitsTracked({ ...habitsTracked, stretch: !habitsTracked.stretch })}
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <Form.Check
-                        type="switch"
-                        id="Meditate"
-                        label="Meditate 20 mins"
-                        name="meditate"
-
-                        checked={habitsTracked.meditate}
-                        onClick={() => setHabitsTracked({ ...habitsTracked, meditate: !habitsTracked.meditate })}
-                    />
-                </Col>
-                <Col>
-                    <Form.Check
-                        type="switch"
-                        id="Coffee"
-                        label="Drink Coffee"
-                        name="coffee"
-
-                        checked={habitsTracked.coffee}
-                        onClick={() => setHabitsTracked({ ...habitsTracked, coffee: !habitsTracked.coffee })}
-                    />
-                </Col>
-            </Row>
-
-        </div>
-    )
-}
-
-export default HabbitToggleList
+export default HabbitToggleList;
