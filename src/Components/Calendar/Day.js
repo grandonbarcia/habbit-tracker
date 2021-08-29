7cab492ad8c71b8dd7879029c29dd0e161591135
Brandon Garcia <grandonleebarcia@gmail.com>
2021-08-29 13:41:13 -0700

    src/Components/Calendar/Day.js

--- src/Components/Calendar/Day.js
+++ src/Components/Calendar/Day.js
@@ -1,41 +1,64 @@
-import React, { useState, useEffect } from 'react'
-import { Button } from 'react-bootstrap'
-import { GiMeditation } from 'react-icons/gi'
-import { BiDumbbell } from 'react-icons/bi'
-import { GrYoga } from 'react-icons/gr'
-import { AiOutlineCoffee } from 'react-icons/ai'
+import React, { useState } from "react";
+import { Row, Col } from "react-bootstrap";
+import { GiMeditation } from "react-icons/gi";
+import { BiDumbbell } from "react-icons/bi";
+import { GrYoga } from "react-icons/gr";
+import { AiOutlineCoffee } from "react-icons/ai";
+import { BsPlusCircleFill } from "react-icons/bs";
+import { FiBook } from "react-icons/fi";
 
+const Days = ({ date, today, showHabitsDone, handleShow }) => {
+  const [displayButton, toggleButton] = useState(false);
 
+  let dataFound = localStorage.getItem(date);
+  let data, meditate, stretch, excercise, coffee, journal;
+  let dateNum = date.split(" ")[2];
 
+  if (dataFound) {
+    data = JSON.parse(dataFound);
+    meditate = data.meditate;
+    stretch = data.stretch;
+    excercise = data.excercise;
+    coffee = data.coffee;
+    journal = data.journal;
+  }
 
-const Days = ({ date, today, showHabitsDone }) => {
+  const style = {
+    height: "30px",
+    width: "30px",
+    marginTop: "auto",
+    marginLeft: "auto",
+    color: "#007BFF",
+  };
 
-    const [isButtonDisplay, toggleButton] = useState(false)
+  return (
+    <td
+      onMouseEnter={() => toggleButton(true)}
+      onMouseLeave={() => toggleButton(false)}
+      onClick={() => showHabitsDone(date)}
+      className="day"
+    >
+      <div className="date">{dateNum}</div>
+      <Row>
+        <Col>
+          <div>{meditate && <GiMeditation />}</div>
+        </Col>
+        <Col>
+          <div>{stretch && <GrYoga />}</div>
+        </Col>
+        <Col>
+          <div>{excercise && <BiDumbbell />}</div>
+        </Col>
+        <Col>
+          <div>{coffee && <AiOutlineCoffee />}</div>
+        </Col>
+        <Col>
+          <div>{journal && <FiBook />}</div>
+        </Col>
+      </Row>
+      {displayButton && <BsPlusCircleFill style={style} onClick={handleShow} />}
+    </td>
+  );
+};
 
-    let dataFound = localStorage.getItem(date);
-    let data, meditate, stretch, excercise, coffee;
-
-    if (dataFound) {
-
-        data = JSON.parse(dataFound);
-        meditate = data.meditate
-        stretch = data.stretch
-        excercise = data.excercise
-        coffee = data.coffee
-
-    }
-
-
-    return (
-        <td onMouseEnter={() => toggleButton(true)} onMouseLeave={() => toggleButton(true)} onClick={() => showHabitsDone(date)} className="day other-month"  >
-            <div className="date">{date.split(' ')[2]}</div>
-            <div>{meditate ? <GiMeditation /> : ''}</div>
-            <div>{stretch ? <GrYoga /> : ''}</div>
-            <div>{excercise ? <BiDumbbell /> : ''}</div>
-            <div>{coffee ? <AiOutlineCoffee /> : ''}</div>
-
-        </td>
-    )
-}
-
-export default Days
+export default Days;
