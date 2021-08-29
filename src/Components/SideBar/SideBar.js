6b1cf1790400760273e699caa472bd1bfa1cf5c9
Brandon Garcia <grandonleebarcia@gmail.com>
2021-08-29 15:52:33 -0700

    removed unnused imported components from react-bootstrap

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Form, Button, Card } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import Instructions from "./Instructions";
 
 const InputPanel = ({ displayDate }) => {
